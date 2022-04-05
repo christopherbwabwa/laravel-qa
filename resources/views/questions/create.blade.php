3d9adb4c8e36345dd90b6f9d668711db779146e7
Christopher Bwabwa <christopherbwabwa@gmail.com>
2022-04-06 01:47:39 +0300

    Implement form validation & store the question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -17,32 +17,32 @@
                     <div class="card-body">
                        <form action="{{ route('questions.store') }}" method="POST">
                             @csrf
-                            
+
                         <div class="form-group">
                                 <label for="question-title">Question Title</label>
-                                <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+                                <input type="text" name="title"  value="{{ old('title') }}" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
 
                                 @if ($errors->has('title'))
                                     <div class="invalid-feedback">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $errors->first('title') }}</strong>
                                     </div>
                                 @endif
                         </div>
 
                         <div class="form-group">
                             <label for="question-body">Explain your question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}">{{ old('body') }}</textarea>
 
                             @if ($errors->has('body'))
                                     <div class="invalid-feedback">
-                                        <strong>{{ $message }}</strong>
+                                        <strong>{{ $errors->first('body') }}</strong>
                                     </div>
                             @endif
 
                         </div>
 
                         <div class="form-group">
-                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask this question</button>
+                            <button type="submit" class="btn btn-outline-primary btn-md">Ask this question</button>
                         </div>
                        </form>
                     </div>
