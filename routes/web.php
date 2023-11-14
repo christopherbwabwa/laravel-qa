fc812a7dca9434b843123fd2664840199ad07911
Christopher Bwabwa <christopherbwabwa@gmail.com>
2023-11-14 13:23:15 +0100

    updating routes & adding readme

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/','QuestionController@index');
+
 Auth::routes( ['verify' => true]);
 
 Route::middleware('verified')->group( function () {
